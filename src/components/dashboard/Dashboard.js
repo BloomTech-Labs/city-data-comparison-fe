80ac7d2399e3f69958071ccc803e4e1972ac9776
admin <admin@admins-MacBook-Pro.local>
2019-12-13 13:06:04 -0800

    skeleton for dashboard search container implemented

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,14 +1,57 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function Dashboard(){
+
+     const [city, setCity] = useState("")
+     
+     const handleChange = (event) => {
+          event.preventDefault();
+          setCity(event.target.value)
+     }
+     
+     const onSubmit = (event) => {
+          event.preventDefault();
+     }
 
 
      return(
           <div className="dashboard-container">
-               <div className="dashboard-search-container"></div>
-               <div className="dashboard-features-container"></div>
-               <div className="dashboard-metrics-container"></div>
-               <div className="dashboard-compare-container"></div>
+               <div className="dashboard-search-container">
+                    {/* <div>map goes here</div> */}
+
+                    <div>
+                         <h1>Choice is YOURS</h1>
+                         <p>Choose the information you want to see about city(ies).</p>
+                         
+                         <form onSubmit={}>
+                              <input 
+                                   type="text"
+                                   name="city"
+                                   value={city}
+                                   onChange={handleChange}
+                                   placeholder="City"
+                              />
+                              <button>\u279E</button>
+                         </form>
+                         <p>OR</p>
+                         <button>Compare cities</button>
+                    </div>
+               </div>
+
+
+               <div className="dashboard-features-container">
+
+               </div>
+
+
+               <div className="dashboard-metrics-container">
+
+               </div>
+
+
+               <div className="dashboard-compare-container">
+
+               </div>
           </div>
      )
 }
