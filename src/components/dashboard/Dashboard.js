3a97ebc6c87c9dfc59974481b2793f3f69ffec16
admin <admin@admins-MacBook-Pro.local>
2019-12-13 15:08:40 -0800

    mock product descriptions implemented

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -43,9 +43,18 @@ function Dashboard(){
 
 
                <div className="dashboard-features-container">
-                    <img className="feature-images" src={data} alt=""/>
-                    <img className="feature-images" src={forsale} alt=""/>
-                    <img className="feature-images" src={insync} alt=""/>
+                    <div className="feature-descriptions">
+                         <img className="feature-images" src={data} alt="data"/>
+                         <p>PHOTOS WILL CHANGE LATER. Lorem ipsum 90's meh master cleanse taxidermy kickstarter quinoa bespoke craft beer single-origin coffee cray PBR&B put a bird on it photo booth. </p>
+                    </div>
+                    <div className="feature-descriptions">
+                         <img className="feature-images" src={forsale} alt="house-forsale"/>
+                         <p>PHOTOS WILL CHANGE LATER. Photo booth bespoke kogi food truck polaroid gentrify post-ironic. Leggings wolf YOLO pork belly, cliche quinoa plaid +1 humblebrag shaman ennui gochujang.</p>
+                    </div>
+                    <div className="feature-descriptions">
+                         <img className="feature-images" src={insync} alt="business-insync"/>
+                         <p>PHOTOS WILL CHANGE LATER. Cold-pressed, art party bespoke heirloom fam vinyl copper mug hexagon pour-over offal. Twee aesthetic leggings, post-ironic waistcoat. </p>
+                    </div>
                </div>
 
 
