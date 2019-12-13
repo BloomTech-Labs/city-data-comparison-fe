a13e39fae57ec6741ed56a206bc435e511e2e4fd
admin <admin@admins-MacBook-Pro.local>
2019-12-13 11:23:32 -0800

    navigation folder and route implemented

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Dashboard from './dashboard/Dashboard'
+import Navigation from './navigation/Navigation'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      
       <h1>City Data Comparison</h1>
 
       {/* route components in here here */}
-      <Route path='/' component={Dashboard} />
+      <Route path='/' component={Navigation} />
+      <Route exact path='/' component={Dashboard} />
     </div>
   );
 }
---
+++ src/navigation/Navigation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Navigation(){
+
+     return(
+          <div>hello I am the navigation</div>
+     )
+}
+export default Navigation;
