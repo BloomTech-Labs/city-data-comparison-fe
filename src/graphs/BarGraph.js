4ea59d57a9c1f4f17ab4df53557626663515c788
Muamer Kukic <kukicako@gmail.com>
2019-12-13 13:15:44 -0800

    Added some base graphs to use later/add data to

---
+++ src/graphs/BarGraph.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import {Bar} from 'react-chartjs-2';
+
+export default class BarGraph extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      chartData: {
+        labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New BedFord'],
+        datasets:[
+          {
+            label:'Population',
+            data:[
+              617594,
+              181045,
+              153060,
+              106519,
+              105162,
+              95072
+            ],
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
+            ]
+          }
+        ]
+      }
+    }
+  }
+  static defaultProps = {
+    displayTitle:true,
+    displayLegend: true,
+    legendPosition:'top',
+    location:'Population'
+  }
+  render(){
+    return (
+      <div className="chart">
+        <Bar
+          data={this.state.chartData}
+          options={{
+            title:{
+              display:this.props.displayTitle,
+              text:'Largest Cities By '+this.props.location,
+              fontSize:25
+            },
+            legend:{
+              display:this.props.displayLegend,
+              position:this.props.legendPosition
+            }
+          }}
+        />
+      </div>
+    )
+  }
+  }
