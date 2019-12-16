6ff319a819e48d288f09db6bf6675b15f1dd5fee
Michael Harms <michael.harms6010@gmail.com>
2019-12-16 17:00:29 -0600

    removed extraneous route, added home link

--- src/App.js
+++ src/App.js
@@ -12,9 +12,10 @@ function App() {
     <div className="App">
       
       <h1>City Data Comparison</h1>
+      <Link to="/">Home</Link>
       <Link to="/map">Map</Link>
       {/* route components in here here */}
-      <Route path='/' component={Navigation} />
+      <Navigation />
       <Route exact path='/' component={Dashboard} />
       <Route path="/map" component={Map} />
     </div>
