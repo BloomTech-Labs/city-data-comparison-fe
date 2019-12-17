99bfe4bb96fcd07f78ee341cbbab476d64e17a5c
admin <admin@admins-MacBook-Pro.local>
2019-12-17 14:33:17 -0800

    framework for footer started

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -4,7 +4,32 @@ function Footer(){
 
      return(
           <div className="footer-container">
-               Hello this is the footer
+               <div>
+                    <div>
+                    <h2>Find Your Best Place To Live</h2>
+                    <button>Get Started</button>
+                    </div>
+                    <div>
+                         <div>
+                              <h3>Services</h3>
+                              <a>Search a City</a>
+                              <a>Compare Cities</a>
+                         </div>
+                         <div>
+                              <h3>Company</h3>
+                              <a>Meet the Team</a>
+                              <a>About Us</a>
+                              <a>Contact Us</a>
+                         </div>
+                         <div>
+                              <h3>Resources</h3>
+                              <a>Data Sources</a>
+                              <a>Private Policy</a>
+                              <a>Terms of Use</a>
+                         </div>
+
+                    </div>
+               </div>
           </div>
      )
 }
