83a89b794b59b4f432ce6fd52776720e583af887
John Watt <j.watt10@gmail.com>
2019-12-19 12:08:03 -0700

    :construction: Saved Cities Axios Call
    Made dummy axios call to get list of saved cities for a user to be displayed. Will update .get when backends for both auth and ds have been deployed

--- src/components/user-profile/Profile.js
+++ src/components/user-profile/Profile.js
@@ -4,18 +4,38 @@ import axios from 'axios'
 import 'profile.scss'
 
 function Profile() {
-
+    //state for logged in user
     const [user, setUser] = useState();
+    console.log('Current user state',user)
+    //state for saved cities for specific user
+    const [savedCities, setSavedCities] = useState([]);
+    console.log('Saved city state',savedCities)
+    
 
+    //User information axios call
     useEffect(() => {
         axios
             .get('')
             .then(res => {
+                console.log('Resonse from user call',res)
                 setUser(res.data)
             })
-            .then(err => {
+            .catch(err => {
                 console.error('Unable to get user information', err);
             });
+    }, []);
+
+    //Users saved cities axios call
+    useEffect(() => {
+        axios
+            .get('')
+            .then(res => {
+                console.log('Response from cities call',res)
+                setSavedCities(res.data)
+            })
+            .catch(err => {
+                console.error('Unable to get saved cities list', err);
+            })
     })
 
 
