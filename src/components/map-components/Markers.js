1bb79bd407b6a5d8ea26f9da48370727d566a3b8
dbriksza <dbriksza@gmail.com>
2019-12-19 14:27:51 -0500

    added a mapbox to show streetview of city

--- src/components/map-components/Markers.js
+++ src/components/map-components/Markers.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import { Marker } from 'react-map-gl';
+import React, { useState } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import styled from "styled-components";
 import { Link } from 'react-router-dom';
 
 import pin from './pin.png';
 import activepin from"./activepin.png";
 
+const SelectedMapWrapper = styled.div`
+  width:10vh;
+  height:10vh;
+`;
 
 const Markers = ({ zoom, cityMarkers, selected, toggleSelected }) => {
 
-
+    const [selectedViewport, setSelectedViewport] = useState({
+        width: '100%',
+        height: '100%',
+        latitude: 45,
+        longitude: -95,
+        zoom: 5,
+        trackResize: false
+      });
 
   return (
         <div>
@@ -16,7 +28,10 @@ const Markers = ({ zoom, cityMarkers, selected, toggleSelected }) => {
             return (
                 <Marker key={cityMarker.lat} latitude={cityMarker.lat} longitude={cityMarker.lng}>
                     {/* <Link className='map-marker' to={`/map/${cityMarker.city}${cityMarker.state_id}`}> */}
-                    <div onClick={() => toggleSelected(cityMarker)}>
+                    <div 
+                    onClick={() => toggleSelected(cityMarker)} 
+                    // onMouseEnter={(e) => (setSelectedViewport({...selectedViewport, latitude:e.target.name.lat, longitude:e.target.name.lng}), console.log(e.target.name))}
+                    >
                         {selected.find(item => item === cityMarker) 
                         ? <img src={activepin} alt={`A map pin indicating ${cityMarker.city}`} />
                         : <img src={pin} alt={`A map pin indicating ${cityMarker.city}`} />}
@@ -26,6 +41,17 @@ const Markers = ({ zoom, cityMarkers, selected, toggleSelected }) => {
                 </Marker>
                 );
         })}
+        <div className="citymap">
+                <SelectedMapWrapper>
+                  <ReactMapGL
+                    mapStyle='mapbox://styles/mapbox/streets-v11'
+                    {...selectedViewport}
+                    mapboxApiAccessToken={
+                    'pk.eyJ1IjoiYnJ1bmNodGltZSIsImEiOiJjazIwdG80MGkxN3lmM25vaWZ5cThkZDU1In0.uYqrXjiEyUL1mTEO_N5-0w'
+                    }
+                  />
+                </SelectedMapWrapper>
+              </div>
     </div>
   );
 };
