98ff79ee2d85b1cdcb08192d4568307c9779cf41
Michael Harms <michael.harms6010@gmail.com>
2019-12-19 20:47:57 -0600

    improves legibility of privacy policy

--- src/legal/PrivacyPolicy.js
+++ src/legal/PrivacyPolicy.js
@@ -7,7 +7,9 @@ const PrivacyPolicy = _ =>
     <h3>Your privacy is critically important to us.</h3>
     Cityvisor.com is located at:<br/>
     <address>
-    Cityvisor.com<br/><br/>5079935257			</address>
+        Cityvisor.com<br/><br/>
+        5079935257			
+    </address>
 
     <p>It is Cityvisor.com's policy to respect your privacy regarding any information we may collect while operating our website. This Privacy Policy applies to <a href="http://cityvisor.com">cityvisor.com</a> (hereinafter, "us", "we", or "cityvisor.com"). We respect your privacy and are committed to protecting personally identifiable information you may provide us through the Website. We have adopted this privacy policy ("Privacy Policy") to explain what information may be collected on our Website, how we use this information, and under what circumstances we may disclose the information to third parties. This Privacy Policy applies only to information we collect through the Website and does not apply to our collection of information from other sources.</p>
     <p>This Privacy Policy, together with the Terms and conditions posted on our Website, set forth the general rules and policies governing your use of our Website. Depending on your activities when visiting our Website, you may be required to agree to additional terms and conditions.</p>
@@ -19,7 +21,7 @@ const PrivacyPolicy = _ =>
     <h2>Gathering of Personally-Identifying Information</h2>
     <p>Certain visitors to Cityvisor.com's websites choose to interact with Cityvisor.com in ways that require Cityvisor.com to gather personally-identifying information. The amount and type of information that Cityvisor.com gathers depends on the nature of the interaction. For example, we ask visitors who sign up for a blog at http://cityvisor.com to provide a username and email address.</p>
 
-                <h2>Security</h2>
+    <h2>Security</h2>
     <p>The security of your Personal Information is important to us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Information, we cannot guarantee its absolute security.</p>
 
     <h2>Advertisements</h2>
