122246cf9a7b62e2ce4ad0e65604203521d10179
umekow <umekolashun@gmail.com>
2020-01-07 11:14:40 -0600

    structure of page

---
+++ src/components/AboutUs.js
@@ -0,0 +1,138 @@
+import React from 'react'; 
+
+const AboutUs = props => {
+    return(
+        <div className="aboutUs">
+
+            
+            <div className="team">
+
+
+                <div className="project">
+
+                    {/*Anna*/}
+                    <div className="member">
+                        <img src="" alt=""/>
+                        <div className="info">
+                            <p className="role"></p>
+                            <p className="about"></p>
+                            <span className="social"></span>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="design">
+                    {/*Ruth*/}
+                    <div className="member">
+                        <img src="" alt=""/>
+                        <div className="info">
+                            <p className="role"></p>
+                            <p className="about"></p>
+                            <span className="social"></span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="ds">
+
+                    {/*Luke*/}
+                    <div className="member">
+                        <img src="" alt=""/>
+                        <div className="info">
+                            <p className="role"></p>
+                            <p className="about"></p>
+                            <span className="social"></span>
+                        </div>
+                    </div>
+
+                    {/*Matthew*/}
+                    <div className="member">
+                        <img src="" alt=""/>
+                        <div className="info">
+                            <p className="role"></p>
+                            <p className="about"></p>
+                            <span className="social"></span>
+                        </div>
+                    </div>
+
+                    {/*Scott*/}
+                    <div className="member">
+                        <img src="" alt=""/>
+                        <div className="info">
+                            <p className="role"></p>
+                            <p className="about"></p>
+                            <span className="social"></span>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="web">
+
+                    {/*Daniel*/}
+                    <div className="member">
+                        <img src="" alt=""/>
+                        <div className="info">
+                            <p className="role"></p>
+                            <p className="about"></p>
+                            <span className="social"></span>
+                        </div>
+                    </div>
+
+                    {/*John*/}
+                    <div className="member">
+                        <img src="" alt=""/>
+                        <div className="info">
+                            <p className="role"></p>
+                            <p className="about"></p>
+                            <span className="social"></span>
+                        </div>
+                    </div>
+
+                    {/*Kevin*/}
+                    <div className="member">
+                        <img src="" alt=""/>
+                        <div className="info">
+                            <p className="role"></p>
+                            <p className="about"></p>
+                            <span className="social"></span>
+                        </div>
+                    </div>
+
+                    {/*Michael*/}
+                    <div className="member">
+                        <img src="" alt=""/>
+                        <div className="info">
+                            <p className="role"></p>
+                            <p className="about"></p>
+                            <span className="social"></span>
+                        </div>
+                    </div>
+
+                    {/*Muamer*/}
+                    <div className="member">
+                        <img src="" alt=""/>
+                        <div className="info">
+                            <p className="role"></p>
+                            <p className="about"></p>
+                            <span className="social"></span>
+                        </div>
+                    </div>
+
+                    {/*Umeko*/}
+                    <div className="member">
+                        <img src="" alt=""/>
+                        <div className="info">
+                            <p className="role"></p>
+                            <p className="about"></p>
+                            <span className="social"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutUs; 
