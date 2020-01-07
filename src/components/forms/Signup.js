bfd229f2c690a3138f0a08ef60d9c283818b7341
umekow <umekolashun@gmail.com>
2020-01-07 16:30:26 -0600

    some of the skeleton done for signup form

--- src/components/forms/Signup.js
+++ src/components/forms/Signup.js
@@ -3,7 +3,18 @@ import React from 'react';
 const Signup = props => {
     return(
         <div className='signup'>
+            <h2>Create Your Account</h2>
+
+            <div className="auth">
+                
+            </div>
             
+            <div className="center-text">
+                <p>or with email</p>
+            </div>
+            <div className="fields">
+                
+            </div>
         </div>
     )
 }
