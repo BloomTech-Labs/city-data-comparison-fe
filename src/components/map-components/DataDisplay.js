3d6700ea92d47729701646c44d9690b4ba58bfd8
theJOHNwatt <j.watt10@gmail.com>
2020-01-08 14:20:42 -0800

    :sparkles: Side Nav
    added toggle function for side nav bar on maps

--- src/components/map-components/DataDisplay.js
+++ src/components/map-components/DataDisplay.js
@@ -1,4 +1,4 @@
-import React from "react";
+import  React, {useState} from "react";
 import {NavLink, Route}  from "react-router-dom";
 
 import Housing from "./Housing";
@@ -13,6 +13,16 @@ import deleteIcon from "./icons/close_red.png";
 
 const DataDisplay = ({search, selected, toggleSelected, onSearch, setSearch, cityMarkers, viewport, setViewport}) => {
 
+    const [menu, setMenu] = useState({status: 'closed'})
+
+    const toggleMenu = () => {
+        if (menu.status === 'closed') {
+            setMenu({...menu, status:'open'}) 
+        } else if (menu.status === 'open') {
+            setMenu({...menu, status:'closed'})
+        }
+    }
+
     const toggleVisibility = city => {
         console.log("toggling visibility of ", city.city)
     }
@@ -20,7 +30,10 @@ const DataDisplay = ({search, selected, toggleSelected, onSearch, setSearch, cit
     return (
         <div className="data-browser">
             <nav className="data-nav">
+                <button className='menu-toggle' onClick={toggleMenu}>X</button>
+                <div className={menu.status}>
                 <div className="data-nav-top">
+                    
                     <MapSearch 
                         setSearch={setSearch}
                         onSearch={onSearch} 
@@ -29,6 +42,7 @@ const DataDisplay = ({search, selected, toggleSelected, onSearch, setSearch, cit
                         viewport={viewport}
                         setViewport={setViewport}   
                     />
+                    
                     <Route path="/map/housing" component={CostNav} />
                     <Route path="/map/jobs" component={JobsNav} />
                     <Route path="/map/culture" component={SafetyNav} />
@@ -40,12 +54,14 @@ const DataDisplay = ({search, selected, toggleSelected, onSearch, setSearch, cit
                         </span>
                     </li>)}
                 </ul>
+                </div>
             </nav>
             <div className="data-by-category">
                 <nav className="data-subnav">
+                   
                     <NavLink activeClassName="selected" to="/map/housing">Housing</NavLink>
                     <NavLink activeClassName="selected" to="/map/jobs">Job Market</NavLink>
-                    <NavLink activeClassName="selected" to="/map/culture">Culture</NavLink>               
+                    <NavLink activeClassName="selected" to="/map/culture">Culture</NavLink>                             
                 </nav>
                 <Route path="/map/housing" render={_ => <Housing selected={selected} /> } />
                 <Route path="/map/jobs" render={_ => <Jobs selected={selected} /> } />
