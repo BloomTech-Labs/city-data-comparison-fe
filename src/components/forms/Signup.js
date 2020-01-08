30cf7387bd6544b8fb35f6b31b712cde29e0d6da
umekow <umekolashun@gmail.com>
2020-01-08 16:23:55 -0600

    set up divs to organize all elements on the page

--- src/components/forms/Signup.js
+++ src/components/forms/Signup.js
@@ -4,17 +4,27 @@ import './forms.scss';
 const Signup = props => {
     return(
         <div className='signup'>
-            <h2>Create Your Account</h2>
+            <div className="form-container">
+                <div className="inner-form">
+                    <div className="form">
+                        <h2>Create Your Account</h2>
 
-            <div className="auth">
-                
-            </div>
-            
-            <div className="center-text">
-                <p>or with email</p>
-            </div>
-            <div className="fields">
-                
+                        <div className="auth">
+                            <button className="google"></button>
+                            
+                        </div>
+                        
+                        <div className="center-text">
+                            <p>or with email</p>
+                        </div>
+                        <div className="fields">
+                            
+                        </div>
+                    </div>
+                    <div className="photo">
+                        {/*photo*/}
+                    </div>
+                </div>
             </div>
         </div>
     )
