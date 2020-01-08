4c699931a782e958ef771d0393de4ad4f47e0fc2
Michael Harms <michael.harms6010@gmail.com>
2020-01-08 13:07:55 -0600

    mapping data to graphs

--- src/components/map-components/Culture.js
+++ src/components/map-components/Culture.js
@@ -3,8 +3,12 @@ import {Route} from "react-router-dom";
 import BarGraph from "../graphs/BarGraph";
 import LineGraph from "../graphs/LineGraph";
 import PieGraph from "../graphs/PieGraph";
+import EducationGraph from "../graphs/EducationGraph";
 
 const Culture = ({selected}) => {
+
+
+
     return (
         <div>
             <h1>City Cultural Statistics:</h1>
@@ -13,7 +17,8 @@ const Culture = ({selected}) => {
 
             <Route path="/map/culture/demographics" component={BarGraph}/>
             <Route path="/map/culture/lifestyle" component={LineGraph}/>
-            <Route path="/map/culture/education" component={PieGraph}/>
+            <Route path="/map/culture/education" render={ _ => <EducationGraph 
+                edData={selected} />}/>
 
         </div>
     );
