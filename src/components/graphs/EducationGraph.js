4d891dc53cdb11c93fd17bb1559080a7fbf6186c
Michael Harms <michael.harms6010@gmail.com>
2020-01-08 17:12:02 -0600

    pares out unused data in graph

--- src/components/graphs/EducationGraph.js
+++ src/components/graphs/EducationGraph.js
@@ -4,43 +4,18 @@ import {Pie} from 'react-chartjs-2';
 export default class EducationGraph extends Component{
   constructor(props){
     super(props);
-    this.state = {
-      chartData: {
-        labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New BedFord'],
-        datasets:[
-          {
-            label:'Population',
-            data:[
-              617594,
-              181045,
-              153060,
-              106519,
-              105162,
-              95072
-            ],
-            backgroundColor:[
-              'rgba(255, 99, 132, 0.6)',
-              'rgba(54, 162, 235, 0.6)',
-              'rgba(255, 206, 86, 0.6)',
-              'rgba(75, 192, 192, 0.6)',
-              'rgba(153, 102, 255, 0.6)',
-              'rgba(255, 159, 64, 0.6)',
-              'rgba(255, 99, 132, 0.6)'
-            ]
-          }
-        ]
-      }
-    }
+   
   }
   static defaultProps = {
     displayTitle:true,
     displayLegend: true,
-    legendPosition:'right',
     location:'Population'
   }
 
  
   render(){
+
+
     return (
       <div className="charts">
         {this.props.edData.map( item => 
