fc93aab9cba81f93115869957da8bcbd89d630a2
dbriksza <dbriksza@gmail.com>
2020-01-08 19:26:16 -0500

    merged to master

--- src/components/map-components/Markers.js
+++ src/components/map-components/Markers.js
@@ -1,21 +1,28 @@
 import React, { useState } from 'react';
 import ReactMapGL, { Marker } from 'react-map-gl';
 
-import PopupMap from './PopupMap';
+import  SelectedMapWrapper  from './PopupMap';
 
 import pin from './icons/pin.png';
 import activepin from"./icons/activepin.png";
 
 const Markers = ({ cityMarkers, selected, toggleSelected }) => {
 
-  const [popState, setPopState] = useState({
-    lat: 1,
-    lng: 1,
-    posleft:0,
-    postop:0,
+  const [selectedViewport, setSelectedViewport] = useState({
+    width: '100%',
+    height: '100%',
+    latitude: 1,
+    longitude: 1,
+    zoom: 13,
+    trackResize: false
+  });
+
+  const [popState, setPopState] = useState ({
+    posleft: 1,
+    postop: 1,
     display: 'none',
-    animation:false
-  })
+    animation: false
+  });
 
   return (
         <div>
@@ -25,8 +32,9 @@ const Markers = ({ cityMarkers, selected, toggleSelected }) => {
                     {/* <Link className='map-marker' to={`/map/${cityMarker.city}${cityMarker.state_id}`}> */}
                   <div
                     onClick={() => toggleSelected(cityMarker)} 
-                    onMouseOver={(e) => (Number.isNaN(parseFloat(e.target.getAttribute("latitude"))) || setPopState({...popState, lat:parseFloat(e.target.getAttribute("latitude")), lng:parseFloat(e.target.getAttribute("longitude")),
-                    posleft:e.pageX, postop:e.pageY, display:'block', animation:true}), console.log(popState))}
+                    onMouseOver={(e) => (Number.isNaN(parseFloat(e.target.getAttribute("latitude"))) || 
+                    setSelectedViewport({...selectedViewport, latitude:parseFloat(e.target.getAttribute("latitude")), longitude:parseFloat(e.target.getAttribute("longitude"))}),
+                    setPopState({...popState, posleft:e.pageX, postop:e.pageY, display:'block', animation:true}), console.log(popState))}
                     onMouseLeave={(e) => setPopState({...popState, display:'none', animation:false})}
                   >
                       {selected.find(item => item === cityMarker) 
@@ -37,7 +45,15 @@ const Markers = ({ cityMarkers, selected, toggleSelected }) => {
               </Marker>
               );
         })}
-         <PopupMap lat={popState.lat} lng={popState.lng} posleft={popState.posleft} postop={popState.postop} display={popState.display} animation={popState.animation}/> 
+         <SelectedMapWrapper left={popState.posleft} top={popState.postop} display={popState.display} animation={popState.animation}>
+            <ReactMapGL 
+              mapStyle='mapbox://styles/mapbox/streets-v11'
+              {...selectedViewport}
+              mapboxApiAccessToken={
+                  'pk.eyJ1IjoiYnJ1bmNodGltZSIsImEiOiJjazIwdG80MGkxN3lmM25vaWZ5cThkZDU1In0.uYqrXjiEyUL1mTEO_N5-0w'
+              }
+            />
+          </SelectedMapWrapper>
       </div>
   );
 };
--- src/components/map-components/PopupMap.js
+++ src/components/map-components/PopupMap.js
@@ -2,61 +2,49 @@ import React, { useState, useEffect } from 'react';
 import ReactMapGL from 'react-map-gl';
 import styled from "styled-components";
 
-const PopupMap = (props) => {
-
-    const SelectedMapWrapper = styled.div`
+const SelectedMapWrapper = styled.div`
         position:fixed;
+        z-index:10;
         transition: height 10s;
         transition: width 2s;
-        width: ${props => props.animation ? "30vh" : "15vh"};
-        height: ${props => props.animation ? "50vh" : "20vh"};
+        left: ${props => props.posleft+10}px;
+        top: ${props => props.postop+10}px;
+        display: ${props => props.display};
+        width: ${props => props.animated ? "30vh" : "30vh"};
+        height: ${props => props.animated ? "50vh" : "50vh"};
     `;
+    export  default SelectedMapWrapper
+// const PopupMap = (props) => {
 
-    const [selectedViewport, setSelectedViewport] = useState({
-        width: '100%',
-        height: '100%',
-        latitude: props.lng,
-        longitude: props.lat,
-        zoom: 13,
-        trackResize: false
-    });
-
-    const [streetViewPos, setStreetViewPos] = useState({
-        posleft: props.posleft,
-        postop: props.postop
-    });
-
+//     // const [streetViewPos, setStreetViewPos] = useState({
+//     //     posleft: props.posleft,
+//     //     postop: props.postop
+//     // });
     
-    useEffect(() => {
-        console.log("effect")
-        // if(props.lat && props.lng && props.posleft && props.postop){
-            setSelectedViewport({
-                ...selectedViewport,
-                latitude: props.lat,
-                longitude: props.lng,
-            })
-            setStreetViewPos({
-                ...streetViewPos,
-                posleft: props.posleft,
-                postop: props.postop,
-                display: props.display
-            })
-        },[props])
+//     // useEffect(() => {
+//     //     console.log("effect")
+//     //     // if(props.lat && props.lng && props.posleft && props.postop){
+//     //         setStreetViewPos({
+//     //             ...streetViewPos,
+//     //             posleft: props.posleft,
+//     //             postop: props.postop,
+//     //             display: props.display
+//     //         })
+//     //     },[props])
 
-    return(    
-        console.log(selectedViewport.latitude, selectedViewport.longitude, streetViewPos),
-        <div className="popupmapparent">
-            <SelectedMapWrapper className="popupmap" id="popupmap"  style={{left:`${streetViewPos.posleft+10}px`, top:`${streetViewPos.postop+10}px`, display:`${streetViewPos.display}`}}>
-                <ReactMapGL 
-                    mapStyle='mapbox://styles/mapbox/streets-v11'
-                    {...selectedViewport}
-                    mapboxApiAccessToken={
-                        'pk.eyJ1IjoiYnJ1bmNodGltZSIsImEiOiJjazIwdG80MGkxN3lmM25vaWZ5cThkZDU1In0.uYqrXjiEyUL1mTEO_N5-0w'
-                    }
-                />
-            </SelectedMapWrapper>
-        </div>
-)
-}
+//     return(    
+//         <div className="popupmapparent">
+//             <SelectedMapWrapper className="popupmap" id="popupmap" >
+//                 {/* <ReactMapGL 
+//                     mapStyle='mapbox://styles/mapbox/streets-v11'
+//                     {...selectedViewport}
+//                     mapboxApiAccessToken={
+//                         'pk.eyJ1IjoiYnJ1bmNodGltZSIsImEiOiJjazIwdG80MGkxN3lmM25vaWZ5cThkZDU1In0.uYqrXjiEyUL1mTEO_N5-0w'
+//                     }
+//                 /> */}
+//             </SelectedMapWrapper>
+//         </div>
+// )
+// }
 
-export default PopupMap;
+// export default PopupMap;
