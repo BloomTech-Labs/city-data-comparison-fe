60238b55cef2e40cb0eb63cd1229d3f9ef55824f
Kevin Nguyen <nguyenkevin225@gmail.com>
2020-01-08 20:39:41 -0800

    fixed footer, but there are bugs for our svg and other photos

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -17,18 +17,18 @@ function Footer(){
 
                     <div className="footer-categories-container">
                          <div className="footer-categories">
-                              <h3>Services</h3>
+                              <h3 className="footer-title">Services</h3>
                               <a href="/">Search a City</a>
                               <a href="/">Compare Cities</a>
                          </div>
                          <div className="footer-categories">
-                              <h3>Company</h3>
+                              <h3 className="footer-title">Company</h3>
                               <a href="/">Meet the Team</a>
                               <a href="/">About Us</a>
                               <a href="/">Contact Us</a>
                          </div>
                          <div className="footer-categories">
-                              <h3>Resources</h3>
+                              <h3 className="footer-title">Resources</h3>
                               <a href="/">Data Sources</a>
                               <a href="/">Private Policy</a>
                               <a href="/">Terms of Use</a>
