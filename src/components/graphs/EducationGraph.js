5e338eadf4b73de934d3b5e65d12703181cd5abc
Michael Harms <michael.harms6010@gmail.com>
2020-01-09 15:45:49 -0600

    removes chart width handlers

--- src/components/graphs/EducationGraph.js
+++ src/components/graphs/EducationGraph.js
@@ -2,14 +2,9 @@ import React, {useState, useEffect} from 'react';
 import {Bar} from 'react-chartjs-2';
 
 export default function EducationGraph (props) {
-  const [chartWidth, setChartWidth] = useState("100%")
-
+  
   const colors = ["red" , "blue" , "green"]
   let index = 0;
-
-  // useEffect( () => {
-  //   setChartWidth((props.edData.length <= 1) ? "100%" : "50%");
-  // }, [props.edData])
 
     return (
       <div className="charts" >
@@ -17,19 +12,20 @@ export default function EducationGraph (props) {
           <div className="chart-container" style={{position: "relative", width: `100%`}}>
             <Bar
               data={{
-                labels: ["9th to 12th grade no diploma", "Associate's degree", "Bachelor's degree", "Graduate degree", "High school", "Less than 9th grade", "Some college no degree"],
+                labels: ["Less than 9th grade",  "Some High School", "High school",  "Some College", "Associate's Degree", "Bachelor's Degree", "Graduate Degree"],
                 datasets: props.edData.map( item => {
                   index = (index + 1) % 3;
                   return {
                     label: item.name.replace(" city" , ""),
                     data: [
+                      item["Educational Attainment"]["Less than 9th grade"],
                       item["Educational Attainment"]["9th to 12th grade no diploma"],
+                      item["Educational Attainment"]["High school"],
+                      item["Educational Attainment"]["Some college no degree"],
                       item["Educational Attainment"]["Associate's degree"],
                       item["Educational Attainment"]["Bachelor's degree"],
-                      item["Educational Attainment"]["Graduate degree"],
-                      item["Educational Attainment"]["High school"],
-                      item["Educational Attainment"]["Less than 9th grade"],
-                      item["Educational Attainment"]["Some college no degree"]
+                      item["Educational Attainment"]["Graduate degree"]                    
+                      
                     ],
                     backgroundColor:
                       colors[index]
