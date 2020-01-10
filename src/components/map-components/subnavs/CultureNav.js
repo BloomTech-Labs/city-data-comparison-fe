1ea6e739788685831499b81fa43266fda29996c5
Michael Harms <michael.harms6010@gmail.com>
2020-01-10 15:15:09 -0600

    specifies link

--- src/components/map-components/subnavs/CultureNav.js
+++ src/components/map-components/subnavs/CultureNav.js
@@ -6,7 +6,7 @@ const SafetyNav = _ => {
     return (
         <>
             <NavLink activeClassName="selected" to="/map/culture/demographics">Demographics</NavLink>
-            <NavLink activeClassName="selected" to="/map/culture/lifestyle">Lifestyle</NavLink>
+            <NavLink activeClassName="selected" to="/map/culture/populationgrowth">Population Growth</NavLink>
             <NavLink activeClassName="selected" to="/map/culture/education">Education</NavLink>
         </>
     )
