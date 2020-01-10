053e66e2ebf5b2b64d55a050cb1b2dd3c264685e
Michael Harms <michael.harms6010@gmail.com>
2020-01-09 21:15:49 -0600

    clears unused import

--- src/components/map-components/Culture.js
+++ src/components/map-components/Culture.js
@@ -2,7 +2,6 @@ import React from "react";
 import {Route} from "react-router-dom";
 import BarGraph from "../graphs/BarGraph";
 import LineGraph from "../graphs/LineGraph";
-import PieGraph from "../graphs/PieGraph";
 import EducationGraph from "../graphs/EducationGraph";
 
 const Culture = ({selected}) => {
