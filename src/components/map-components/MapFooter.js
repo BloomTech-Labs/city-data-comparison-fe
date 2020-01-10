93d837f4b5051ca2d88774da62703b9e5c9baf2f
Michael Harms <michael.harms6010@gmail.com>
2020-01-10 09:59:55 -0600

    fixes typo, removes redundant categories in mapfooter - tm needs better spacing

--- src/components/map-components/MapFooter.js
+++ src/components/map-components/MapFooter.js
@@ -14,20 +14,17 @@ function Footer(){
 
                     <div className="footer-categories-container">
                          <div className="footer-categories">
-                              <h3>Services</h3>
-                              <a href="/">Search a City</a>
-                              <a href="/">Compare Cities</a>
                          </div>
                          <div className="footer-categories">
                               <h3>Company</h3>
                               <a href="/">Meet the Team</a>
-                              <a href="/">About Us</a>
+                              <a href="/">Product Roadmap</a>
                               <a href="/">Contact Us</a>
                          </div>
                          <div className="footer-categories">
                               <h3>Resources</h3>
                               <a href="/">Data Sources</a>
-                              <a href="/">Private Policy</a>
+                              <a href="/">Privacy Policy</a>
                               <a href="/">Terms of Use</a>
                          </div>
                     </div>
