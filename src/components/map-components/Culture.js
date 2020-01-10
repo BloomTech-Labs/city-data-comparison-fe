ddb39381faeccb1c7526a9ef592d5bb60e70ac1a
Michael Harms <michael.harms6010@gmail.com>
2020-01-10 14:29:38 -0600

    adds ethnicity graph

--- src/components/map-components/Culture.js
+++ src/components/map-components/Culture.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Route} from "react-router-dom";
 
 import BarGraph from "../graphs/culture/AgeDistrubution";
-import Ethnicity from "../graphs/culture/ethnicity_graph";
+import EthnicityGraph from "../graphs/culture/EthnicityGraph";
 import Population from "../graphs/culture/PopulationGrowth";
 
 import EducationGraph from "../graphs/EducationGraph";
@@ -16,7 +16,7 @@ const Culture = ({selected}) => {
             <h1>City Cultural Statistics:</h1>     
 
             <Route path="/map/culture/crime" component={() => <BarGraph selected = {selected} />} />
-            <Route path="/map/culture/demographics" component={() => <Ethnicity selected = {selected} />} />
+            <Route path="/map/culture/demographics" render={() => <EthnicityGraph ethData = {selected} />} />
             <Route path="/map/culture/lifestyle" component={() => <Population selected = {selected} />} />
             <Route path="/map/culture/education" render={ _ => <EducationGraph 
                 edData={selected} 
