b98d633585439a418fb6162d65b099b6c689557b
Michael Harms <michael.harms6010@gmail.com>
2020-01-11 12:26:51 -0600

    removes unused autofill component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,7 +1,6 @@
 import React, {useState, useContext} from 'react'
 import {Link} from "react-router-dom"
 
-import PlacesAutocomplete from 'react-places-autocomplete';
 import graph from './assets/graph.svg'
 import pointer from './assets/pointer.svg'
 import location from './assets/location.svg'
