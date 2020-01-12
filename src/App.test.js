d7d109cebdf9747474152c19b5b3e61e0417b080
Michael Harms <michael.harms6010@gmail.com>
2020-01-12 17:55:02 -0600

    adds some render tests for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,18 @@ import App from './App';
 
 test('renders dashboard', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Privacy Policy/i);
-  expect(linkElement).toBeInTheDocument();
+  const cta = getByText(/Start Now/i);
+  expect(cta).toBeInTheDocument();
+});
+
+test('renders footer', () => {
+  const { getByText } = render(<App />);
+  const footerLink = getByText(/Privacy Policy/i);
+  expect(footerLink).toBeInTheDocument();
+});
+
+test('renders navigation', () => {
+  const { getByText } = render(<App />);
+  const mapLink = getByText(/Menu/i);
+  expect(mapLink).toBeInTheDocument();
 });
