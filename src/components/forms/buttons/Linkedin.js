a7d4f5933f25f3e5149b5658d1f5f25bf074d439
umekow <umekolashun@gmail.com>
2020-01-14 13:38:31 -0600

    linkedin button and padding'

---
+++ src/components/forms/buttons/Linkedin.js
@@ -0,0 +1,19 @@
+import React, {useEffect} from 'react'; 
+import axios from 'axios'; 
+
+//stylesheet
+import './styles/facebook.scss'
+//assets
+import linkedin from '../../../assets/icons/Linkedin.svg'; 
+
+const Linkedin = props => {
+
+    return (
+        <div className='linkedin-button'>
+            <img className="li-icon" src={linkedin} alt="linkedin icon"/>
+            <p className="linkedin-name">{props.action} with Linkedin</p>
+        </div>
+    )
+}
+
+export default Linked; 
