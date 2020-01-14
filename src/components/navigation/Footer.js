96a8865768aa5bf8e48fef2acc18774bc38ce4bc
dbriksza <dbriksza@gmail.com>
2020-01-14 17:48:58 -0500

    initialized branch file structure

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -1,46 +1,58 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
-
+import React, { useState } from 'react';
+import Modal from "../modal/modal"
+import useModal from "../modal/useModal"
+import "../modal/modal.scss";
+import AboutUs from "../aboutus/AboutUs"
 
 function Footer(){
 
+     const {isShowing, toggle} = useModal();
+     // const {modalState, setModalState} = useState({});
+
      return(
-          <div className="footer-container">
-               <div className="footer">
-                    <div className="footer-CTA">
-                         <div className="footer-CTA-description">
-                              <p>Find Your Best Place To Live</p>
-                              <button className="footer-CTA-button">Start Now</button>
+          <>
+               <Modal
+                    isShowing={isShowing}
+                    hide={toggle}
+                    component={<AboutUs/>}
+               />
+               <div className="footer-container">
+                    <div className="footer">
+                         <div className="footer-CTA">
+                              <div className="footer-CTA-description">
+                                   <p>Find Your Best Place To Live</p>
+                                   <button className="footer-CTA-button">Start Now</button>
+                              </div>
+                              <div>
+                                   <p className="copyright">© City Data Comparison 2019</p>
+                              </div>
+                         </div>
+
+                         <div className="footer-categories-container">
+                              <div className="footer-categories">
+                                   <h3 className="footer-title">Services</h3>
+                                   <a href="/">Search a City</a>
+                                   <a href="/">Compare Cities</a>
+                              </div>
+                              <div className="footer-categories">
+                                   <h3 className="footer-title">Company</h3>
+                                   <a href="/">Meet the Team</a>
+                                   <a onClick={toggle}>About Us</a>
+                                   <a href="/">Contact Us</a>
+                              </div>
+                              <div className="footer-categories">
+                                   <h3 className="footer-title">Resources</h3>
+                                   <a href="/">Data Sources</a>
+                                   <a href="/">Private Policy</a>
+                                   <a href="/">Terms of Use</a>
+                              </div>
                          </div>
                          <div>
-                              <p className="copyright">© City Data Comparison 2019</p>
+                              <p className="copyright-mobile">© City Data Comparison 2019</p>
                          </div>
-                    </div>
-
-                    <div className="footer-categories-container">
-                         <div className="footer-categories">
-                              <h3 className="footer-title">Services</h3>
-                              <Link to="/map">Search a City</Link>
-                              <Link to="/map">Compare Cities</Link>
-                         </div>
-                         <div className="footer-categories">
-                              <h3 className="footer-title">Company</h3>
-                              <Link to="/aboutus">Meet the Team</Link>
-                              <Link to="/aboutus">About Us</Link>
-                              <Link to="/">Contact Us</Link>
-                         </div>
-                         <div className="footer-categories">
-                              <h3 className="footer-title">Resources</h3>
-                              <Link to="/">Data Sources</Link>
-                              <Link to="/privacypolicy">Privacy Policy</Link>
-                              <Link to="/">Terms of Use</Link>
-                         </div>
-                    </div>
-                    <div>
-                         <p className="copyright-mobile">© City Data Comparison 2019</p>
                     </div>
                </div>
-          </div>
+          </>
      )
 }
 
