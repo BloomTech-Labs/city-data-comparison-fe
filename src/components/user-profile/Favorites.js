7a2a81bc7adf3c735c7810f4f0479240ee7f8bdb
theJOHNwatt <j.watt10@gmail.com>
2020-01-15 12:49:45 -0800

    moved saved favorite city to favorites component. set a toggle state for profile

--- src/components/user-profile/Favorites.js
+++ src/components/user-profile/Favorites.js
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 
 const Favorites = ()=> {
 
+    //state for saved cities for specific user
+    const [savedCitiesUrl, setSavedCitiesUrl] = useState();
+    const [savedCities, setSavedCities] = useState([]);
+    console.log('Saved cities url',savedCitiesUrl)
+    console.log('Saved cities array state',savedCities)
+
+        //Users saved cities axios call
+        useEffect(() => {
+            axios
+                .get('')
+                .then(res => {
+                    console.log('Response to get users saved cities urls',res)
+                    setSavedCitiesUrl(res.data)
+                    //Once savedCitiesUrl is set, this call will take that information and call our DS db for the acutal information for each city
+                    axios
+                        .get(setSavedCitiesUrl)
+                        .then(res => {
+                            console.log('Response from getting array of all saved cities', res)
+                            setSavedCities(res.data)
+                        })
+                        .catch(err => {
+                            console.error('Error calling DS db', err)
+                        })
+                })
+                .catch(err => {
+                    console.error('Unable to get saved cities list', err);
+                })
+        }, []);
+
+        
+    //delete saved city handler
+    const handleDelete = e => {
+        e.preventDefault();
+        axios
+            .delete(``)
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.error('Error removing city', err);
+            })
+    };
 
     return (
         <div>
--- src/components/user-profile/Profile.js
+++ src/components/user-profile/Profile.js
@@ -15,11 +15,17 @@ function Profile({selected}) {
     const {user, setUser} = useContext(CityContext);
     console.log('Current user state',user)
 
-    //state for saved cities for specific user
-    const [savedCitiesUrl, setSavedCitiesUrl] = useState();
-    const [savedCities, setSavedCities] = useState([]);
-    console.log('Saved cities url',savedCitiesUrl)
-    console.log('Saved cities array state',savedCities)
+    // Edit state toggle
+
+    const {edit, setEdit} = useState({status:'open'})
+
+    const toggleMenu = () => {
+        if (edit.status === 'closed') {
+            setEdit({...edit, status:'open'}) 
+        } else if (edit.status === 'open') {
+            setEdit({...edit, status:'closed'})
+        }
+    }
     
 
     //User information axios call
@@ -35,41 +41,8 @@ function Profile({selected}) {
             });
     }, []);
 
-    //Users saved cities axios call
-    useEffect(() => {
-        axios
-            .get('')
-            .then(res => {
-                console.log('Response to get users saved cities urls',res)
-                setSavedCitiesUrl(res.data)
-                //Once savedCitiesUrl is set, this call will take that information and call our DS db for the acutal information for each city
-                axios
-                    .get(setSavedCitiesUrl)
-                    .then(res => {
-                        console.log('Response from getting array of all saved cities', res)
-                        setSavedCities(res.data)
-                    })
-                    .catch(err => {
-                        console.error('Error calling DS db', err)
-                    })
-            })
-            .catch(err => {
-                console.error('Unable to get saved cities list', err);
-            })
-    }, []);
 
-    //delete saved city handler
-    const handleDelete = e => {
-        e.preventDefault();
-        axios
-            .delete(``)
-            .then(res => {
-                console.log(res);
-            })
-            .catch(err => {
-                console.error('Error removing city', err);
-            })
-    };
+
 
 
     return (
