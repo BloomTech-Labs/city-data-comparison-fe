2dbc5496e0c08ab83a9247aed9706e568045da1b
Michael Harms <michael.harms6010@gmail.com>
2020-01-14 22:12:11 -0600

    removes zombie code

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -374,30 +374,3 @@ function Dashboard(){
 }
 
 export default Dashboard;
-
-
-
-
-
-// PROTOTYPE AUTOFILL FUNCTION
- {/* <div>
-                    <PlacesAutocomplete value={search} onChange={setSearch} onSelect={handleSelect}>
-                         {
-                              ({ getInputProps, suggestions, getSuggestionItemProps, loading })=>(
-                              <div>
-                                   <input {...getInputProps({placeholder: "Type address"})}/>
-                                   <div>
-                                        {loading ? <div>...loading</div> : null}
-
-                                        {suggestions.map( (suggestion) => {
-                                             const style = {
-                                                  backgroundColor: suggestion.active ? "#41b6e6" : "#fff"
-                                             }
-
-                                             return <div {...getSuggestionItemProps(suggestion, {style})}>{suggestion.description}</div>
-                                        })}
-                                   </div>
-                              </div>)
-                         }
-                    </PlacesAutocomplete>
-               </div> */}
