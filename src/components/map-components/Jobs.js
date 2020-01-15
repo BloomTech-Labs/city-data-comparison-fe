9c3a3da48cd0f056f42a22604ba5d32c6ff89b7a
Michael Harms <michael.harms6010@gmail.com>
2020-01-14 19:18:29 -0600

    removes condtional rendering in jobs component

--- src/components/map-components/Jobs.js
+++ src/components/map-components/Jobs.js
@@ -9,9 +9,9 @@ const Jobs = ({selected}) => {
     return (
         <div>
             <h1>Job Market Breakdown:</h1>
-            <Route path="/map/jobs/commute" render={ _ => <Commute edData={selected} />} />
-            <Route path="/map/jobs/jobs" render={ _ => <Industry edData={selected} />} />
-            <Route path="/map/jobs/standards"  render={ _ => <BarGraph edData={selected} />} />
+            <Commute edData={selected} />
+            <Industry edData={selected} />
+            <BarGraph edData={selected} />
         </div>
     );
   };
