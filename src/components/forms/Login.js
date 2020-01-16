b44a6b37adaa27139d144767df41c1ae6e4edaee
umekow <umekolashun@gmail.com>
2020-01-16 14:25:15 -0600

    started form validation

--- src/components/forms/Login.js
+++ src/components/forms/Login.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'; 
+import React, {useState, useEffect} from 'react'; 
 
 
 import city from '../../assets/forms/sf_sign_in.png'
@@ -14,7 +14,34 @@ import Linkedin from './buttons/Linkedin'
 
 const Login = props => {
 
+   const [validated, validate] = useState(false); 
+   const [emailError, setEmailError] = useState(''); 
+   const [passwordError, setPasswordError] = useState('')
+   const [isLoading, setIsLoading] = useState(false); 
    
+    useEffect(() => {
+
+        const validateLogin = () => {
+            if (validated) {
+                //go to next page
+            }else {
+                var email = document.forms["fields"]['email'].value;
+                var password = document.forms['fields']['password'].value;
+
+                if (email === '') {
+                    setEmailError('Please enter your email'); 
+                    return;
+                }
+
+                if (password === '') {
+                    setPasswordError("Please enter your password")
+                }
+                
+            }
+
+        }
+    }, [validated])
+    
     return(
              <div className='login'>
                   
@@ -36,14 +63,16 @@ const Login = props => {
                        <p className="center">or with email</p>
                        <div className="line"></div>
                    </div>
-                   <div className="fields">
+                   <form className="fields">
+                       <p className='error'>{emailError}</p>
                        <input className="email" type='text' name='email' placeholder="Email"/>
+                       <p>{passwordError}</p>
                        <input className="password" type='text' name='password' placeholder="Password"/>
                        
                        
                        <div className="login-button">Start exploring cities</div>
                        <p className='question'>Have an account? <span>Sign in</span> to explore cities</p>
-                   </div>
+                   </form>
                </div>
                <div className="login-photo">
                    {/*photo*/}
