48b799aa7b2b9ec81c903acbcb3b78497d7146ed
umekow <umekolashun@gmail.com>
2020-01-23 12:44:19 -0600

    working on city selections and import app icons

--- src/components/map-components/FavoriteButton.js
+++ src/components/map-components/FavoriteButton.js
@@ -1,7 +1,30 @@
 import React, {useState} from 'react'; 
 
 //media
-import heart_icon from './icons/heart.svg'
+import heart_icon from './icons/heart.svg';
+//delete icon
+import delete_icon from './icons/close_red.png'; 
+
+//add icon
+import add
+//city pointer icon
+import pointer from '../dashboard/assets/pointer.svg'
+
+
+
+const citySelection = props => {
+    //state is used to toggle between adding or removing a city to user's favorites
+    const [add, setAdd] = useState(true); 
+
+    return(
+        <div className='favoriteCitiesSelection'>
+            <img src={pointer} alt={props.city}/>
+            <p>{props.city}</p>
+            <img src={(add) ? }
+        </div>
+    )
+}
+
 const FavoriteButton = props => {
 
     const [selected, setSelected] = useState([]); 
@@ -10,10 +33,13 @@ const FavoriteButton = props => {
         <div className="heart-button" style={{
             'display': 'flex',
             'alignItems' : 'center',
-            'justifyContent' : 'spaceAround'
+            'justifyContent' : 'spaceAround',
+            'background' : 'white'
         }}>
             <img src={heart_icon} alt='add to favorites'/>
             Favorites
         </div>
     )
 }
+
+export default FavoriteButton; 
