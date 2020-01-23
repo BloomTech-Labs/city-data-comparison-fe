f81849fe228752245614d5a4423837206fe13fc9
umekow <umekolashun@gmail.com>
2020-01-23 16:46:21 -0600

    added axios call for fav cities button

--- src/components/map-components/FavoriteButton.js
+++ src/components/map-components/FavoriteButton.js
@@ -11,13 +11,14 @@ import Axios from 'axios';
 
 const FavoriteButton = props => {
 
-    const {selected} = useContext(CityContext);
-    const [hover, setHover] = useState(false);
-    const [saving, setSaving] = useState(false); 
-
+    const {selected} = useContext(CityContext)
+    const [hover, setHover] = useState(false)
+    const [saving, setSaving] = useState(false)
+    const id = sessionStorage.getItem('userId')
+    
     const saveFavorites = () => {
         Axios
-            .post(`https://citrics-staging.herokuapp.com/api/favs`, selected)
+            .post(`https://citrics-staging.herokuapp.com/api/favs/${id}`, selected)
             .then(response => {
                 
                 console.log(response)
