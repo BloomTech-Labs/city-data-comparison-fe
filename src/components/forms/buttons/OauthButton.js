9fe6f74328656edd5c909f053db704e65e4b6a09
umekow <umekolashun@gmail.com>
2020-01-25 20:35:58 -0600

    removed axios call and replaced with redirect instead

--- src/components/forms/buttons/OauthButton.js
+++ src/components/forms/buttons/OauthButton.js
@@ -1,26 +1,19 @@
-import React, { useState} from 'react'; 
+import React from 'react'; 
 import axios from 'axios'; 
 
-//stylesheet
-import './styles/google.scss'
 //assets
 import google from `../../../assets/icons/${props.party}.svg`; 
 
 const OauthButton = props => {
 
-        const connectAccount = () => {
-            axios
-                .get(`https://citrics-staging.herokuapp.com/api/auth/${props.action}/${props.party}`)
-                .then(res => {
-                    console.log(res)
-                })
-                .catch(error => console.log(error))
-        }    
+        const connectAccount = url => window.location.replace(url);  
 
     return (
         <div className={`${props.party}Button`} onClick={() => {connectAccount()}}>
-            <img className={`${props.party}.icon`}src={google} alt={`${props.party} icon`}/>
-            <p className={`${props.party}-name`}>{props.action} with {props.party}</p>
+            <div className='iconContainer'>
+                <img className={`${props.party}.icon`}src={google} alt={`${props.party} icon`}/>
+            </div>
+            <p className={`${props.party}Name`}>{props.action} with {props.party}</p>
         </div>
     )
 }
