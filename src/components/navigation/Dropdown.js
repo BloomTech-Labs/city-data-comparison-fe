1360d6b32f2ca9160caa3514481ff21c4bc948f4
dbriksza <dbriksza@gmail.com>
2020-01-29 15:45:36 -0500

    fixed dropdown styling

--- src/components/navigation/Dropdown.js
+++ src/components/navigation/Dropdown.js
@@ -34,7 +34,7 @@ const DropMenu = (props) => {
                 {/* styling should be abstracted to app.scss or a new scss */}
                 {/* the transparent border top is so you don't leave the div and trigger the toggle */}
                 <div style={{position: "absolute", borderTop: "75px solid transparent", top: "0%", transform: dropdownOpen ? "scaleY(1)" : "scaleY(0)", transformOrigin: "50% 75px", transition: "transform .3s"}}>
-                    <div style={{backgroundColor: "#ceeaee", border: "1px solid #ffd372"}}>
+                    <div style={{backgroundColor: "white", border: "1px solid #2ca4fc"}}>
                         <div style={{padding: "5px"}}>Welcome!</div>
                         <div style={{padding: "5px", backgroundColor: "#73c595", borderRadius: "5px", margin: "5px"}}>
                             <Link to="/profile" style={{color: "white"}}>Profile</Link>
