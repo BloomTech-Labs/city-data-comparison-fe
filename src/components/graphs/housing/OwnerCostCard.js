27fa27928af5e222ba1bdea1fb0c64b9d70341b6
muamer <kukicako@gmail.com>
2020-01-30 17:50:42 -0800

    Avg Max and Module almost done

--- src/components/graphs/housing/OwnerCostCard.js
+++ src/components/graphs/housing/OwnerCostCard.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from "styled-components";
 import helpCircle from '../../map-components/assets/helpcircle.svg'
+import SmocModal from "../../modal/smocModule";
+import useModal from "../../modal/useModal";
+import Smoc from "../../modal/SmocCard";
 
 function TotalPopulation({ethData}) {
+    const {isShowing, toggle} = useModal();
+    const [modalState, setModalState] = useState();
+
     function numberCommas(x) {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
 	return (
+        <>
+               <SmocModal
+                    isShowing={isShowing}
+                    hide={toggle}
+                    component={modalState}
+               />
 		<div className="homeowner-container">
             <p className="chart-title home-title">Homeowner costs</p>
             {ethData.map(item => 
                 <div key={item._id} className="mortgage">
-                    <div className="smoc">SMOC <a href="https://factfinder.census.gov/help/en/selected_monthly_owner_costs.htm"><img className="smoc-img"src={helpCircle} alt='smoc reroute'/></a></div>
+                    <div className="smoc">SMOC 
+                    <a href="#" onClick={() => (setModalState(<Smoc/>), toggle())} style={{cursor: "pointer"}}>
+                        <img className="smoc-img"src={helpCircle} alt='smoc reroute'/></a>
+                    
+                    </div>
+
+
+
                     <div className="mortgage-label-container"><span className="mortgage-label">Mortgage: </span><span className="mortgage-price">${numberCommas(item["Median Selected Monthly Owner Costs with Mortgage"])}<span className="month-label">/mo</span></span></div>
                     <div className="mortgage-label-container"><span className="mortgage-label">No Mortgage:</span><span className="mortgage-price">${numberCommas(item["Median Selected Monthly Owner Costs without Mortgage"])}<span  className="month-label">/mo</span></span></div>
                     <div className="city-mortgage-title">{item["City"]}</div>
@@ -19,6 +38,7 @@ function TotalPopulation({ethData}) {
             )}
 			
 		</div>
+        </>
 	);
 }
 
