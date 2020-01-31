394bcc3907ec5583dfa463108150ee136f0a6f60
thejohnwatt <j.watt10@gmail.com>
2020-01-31 03:47:48 -0800

    styles for burger added from data displayed and refactored to new divs

--- src/components/user-profile/SideNav.js
+++ src/components/user-profile/SideNav.js
@@ -17,18 +17,22 @@ const SideNav = () => {
 
 
     return(
-        <div className='side-nav'>
+        <div className={`side-nav ${burger.status}`}>
             <div className= {`burger-menu ${burger.status}`} onClick={toggleBurger}>
                         <div className="bar1" key="b1" />
                         <div className="bar2" key="b2" />
                         <div className="bar3" key="b3" />
                     </div>
+                    <div className={`slider ${burger.status}`}>
+                    <div className={`menu-items ${burger.status}`}>
             <div className='profile-tabs'>
-                <nav className='nav-items'>
+                    <nav className={`nav-items ${burger.status}`}>
                     <NavLink activeClassName='selected' to='/profile/'><img src={User} alt='profile-icon' />Profile</NavLink>
                     {/* <NavLink activeClassName='selected' to='/profile/preferences'>Preferences</NavLink> */}
                     <NavLink activeClassName='selected' to='/profile/favorites'><img src={Heart} alt='favorites-icon'/>Favorites</NavLink>        
                 </nav>
+            </div>
+            </div>
             </div>
         </div>
     )
