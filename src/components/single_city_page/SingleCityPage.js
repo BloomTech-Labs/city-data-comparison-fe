39edd88f69f99963559b20e739f4a55bb5c28c3c
Anisha Sunkerneni <anishasunkerneni@Anishas-MacBook-Air.local>
2020-03-26 11:31:34 -0700

    updated default scp rendering

--- src/components/single_city_page/SingleCityPage.js
+++ src/components/single_city_page/SingleCityPage.js
@@ -581,7 +581,7 @@ const SingleCityPage = (props) => {
                         <SCPparks data={yelp.parks} />
                         : null}
 
-                    {!defaultDisplay || categories.Activities ? 
+                    {categories.Activities ? 
                         <SCPactivities data={yelp.active} />
                         : null}
 
