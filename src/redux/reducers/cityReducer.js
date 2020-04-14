1c48dd08eae2f2bbbfc7530eb3e8847619e3fb24
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-04-14 17:20:34 -0400

    Imported action types in cityReducer

--- src/redux/reducers/cityReducer.js
+++ src/redux/reducers/cityReducer.js
@@ -1,21 +1,26 @@
+import {
+  FETCH_CITIES,
+  FETCH_CITIES_SUCCESS,
+  SET_CITIES_ERROR,
+} from "../actions/cityActions.js";
 
 let initialState = {
-    selected: [],
-    viewport: {
-        width: "100%",
-        height: "100%",
-        longitude: -96.7,
-        latitude: 38.55,
-        zoom: 3.55,
-        minZoom: 3.5,
-        maxZoom: 10,
-        trackResize: true,
-      }
-}
+  selected: [],
+  viewport: {
+    width: "100%",
+    height: "100%",
+    longitude: -96.7,
+    latitude: 38.55,
+    zoom: 3.55,
+    minZoom: 3.5,
+    maxZoom: 10,
+    trackResize: true,
+  },
+};
 
 export default function cityReducer(state = initialState, action) {
-    switch (action.type) {
-        default:
-            return state
-    }
+  switch (action.type) {
+    default:
+      return state;
+  }
 }
