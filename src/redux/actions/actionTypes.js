89ae55c302fa6f5c6e0e41d6227e7eb9fcc55fbc
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-04-16 12:22:54 -0400

    Added action types remove city and clear cities

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -6,3 +6,6 @@ export const GET_CITY_ERROR = "GET_CITY_ERROR";
 export const GET_CITIES = "GET_CITIES";
 export const GET_CITIES_SUCCESS = "GET_CITIES_SUCCESS";
 export const GET_CITIES_ERROR = "GET_CITIES_ERROR";
+
+export const REMOVE_CITY = "REMOVE_CITY";
+export const CLEAR_CITIES = "CLEAR_CITIES";
