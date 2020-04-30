a8b9dfa084c06ad63e48a751595c0d5fb82dc902
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-04-30 13:47:35 -0400

    removes old unusued tests from App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,20 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders dashboard', () => {
-  const { getByText } = render(<App />);
-  const cta = getByText(/Start Now/i);
-  expect(cta).toBeInTheDocument();
-});
-
-test('renders footer', () => {
-  const { getByText } = render(<App />);
-  const footerLink = getByText(/Privacy Policy/i);
-  expect(footerLink).toBeInTheDocument();
-});
-
-test('renders navigation', () => {
-  const { getByText } = render(<App />);
-  const mapLink = getByText(/Menu/i);
-  expect(mapLink).toBeInTheDocument();
-});
+describe('App.js', () => {
+  it.todo('renders dashboard')
+  it.todo('renders footer')
+  it.todo('renders navigation')
+})
