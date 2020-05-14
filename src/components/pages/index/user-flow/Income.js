c05df19e658dd16ad4f2eb83bf366a353191d5a1
svyatoslav-okshin <svyatokshin@gmail.com>
2020-05-14 18:50:51 -0400

    converted ant design radio buttons into material-ui buttons

--- src/components/pages/index/user-flow/Income.js
+++ src/components/pages/index/user-flow/Income.js
@@ -1,9 +1,11 @@
 import React, {useState} from "react";
 
-import { Radio } from "antd";
-
 export default function Income(props) {
   const [income, setIncome] = useState("");
+
+  const myStyle = {
+    width: "265px"
+  }
 
   const handleChange = (e) => {
     setIncome(e.target.value);
@@ -27,24 +29,18 @@ export default function Income(props) {
 
   return (
     <div>
-      <h2>What is your expected monthly income?</h2>
-      <Radio.Group defaultValue="low" buttonStyle="solid" onChange={onChange}>
-        <Radio.Button value="low" style={{ width: "265px" }}>
+      <h2>What is your expected yearly income?</h2>
+
           Low ($0 - $31,000)
-        </Radio.Button>
-        <Radio.Button value="lower-middle" style={{ width: "265px" }}>
+ 
           Lower-Middle ($31,001 - $49,999)
-        </Radio.Button>
-        <Radio.Button value="middle" style={{ width: "265px" }}>
+
           Middle ($50,000 - $99,999)
-        </Radio.Button>
-        <Radio.Button value="upper-middle" style={{ width: "265px" }}>
+
           Upper-Middle ($100000 - $349,999)
-        </Radio.Button>
-        <Radio.Button value="high" style={{ width: "265px" }}>
+
           High ($350,000 - $723,000)
-        </Radio.Button>
-      </Radio.Group>
+
     </div>
   );
 }
