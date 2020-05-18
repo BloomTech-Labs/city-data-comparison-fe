a71bafd606ddbf2e0b6b09005eb08bc3f972d8ec
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-05-18 13:17:43 -0400

    removes unused imports

--- src/components/pages/comparison/buttons/DeselectCityButton.js
+++ src/components/pages/comparison/buttons/DeselectCityButton.js
@@ -1,35 +1,39 @@
-import React, {useState, useEffect} from 'react'
+import React from "react";
 import { useDispatch } from "react-redux";
-import {removeCity} from '../../../../redux/actions/cityActions.js'
-import closeIcon from '../assets/close.svg';
+import { removeCity } from "../../../../redux/actions/cityActions.js";
+import closeIcon from "../assets/close.svg";
 
-
-const DeselectCityButton = ({city}) => {
-    const dispatch = useDispatch();
-    const handleClick = () => {
-        dispatch(removeCity(city["_id"]))
-    }
-    return (
-        <div style={{
-            'display': 'flex',
-            'cursor' : 'pointer',
-            'alignItems' : 'center',
-            'justifyContent' : 'spaceAround',
-            'background' : 'white',
-            'width': '1.4rem',
-            'justifyContent': 'space-around', 
-            'borderRadius': '0.3rem',
-            'margin': '0 0.3rem',
-            'marginRight': '0.6rem'
-        }}  
-        >
-        <img alt={'Deselect city.'} style={{
-            'width': '100%',
-            'opacity': '84%',
-            'color':''
-        }} src={closeIcon} onClick={handleClick}
-            />
-        </div>
-    )
-}
-export default DeselectCityButton
+const DeselectCityButton = ({ city }) => {
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    dispatch(removeCity(city["_id"]));
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        cursor: "pointer",
+        alignItems: "center",
+        justifyContent: "spaceAround",
+        background: "white",
+        width: "1.4rem",
+        justifyContent: "space-around",
+        borderRadius: "0.3rem",
+        margin: "0 0.3rem",
+        marginRight: "0.6rem",
+      }}
+    >
+      <img
+        alt={"Deselect city."}
+        style={{
+          width: "100%",
+          opacity: "84%",
+          color: "",
+        }}
+        src={closeIcon}
+        onClick={handleClick}
+      />
+    </div>
+  );
+};
+export default DeselectCityButton;
