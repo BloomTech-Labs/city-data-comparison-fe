cffa81d3467cd46aa8344c3bcac5d8d2093ee6f9
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-05-18 13:43:34 -0400

    fixes syntax warning

--- src/components/pages/aboutus/AboutUs.js
+++ src/components/pages/aboutus/AboutUs.js
@@ -1,79 +1,86 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 //file that contains information for each user
-import team from './team';
-import TeamCard from './TeamCard';
-import Footer from '../../navigation/Footer';
+import team from "./team";
+import TeamCard from "./TeamCard";
+import Footer from "../../navigation/Footer";
 
 //styling
-import './aboutus.scss'; 
+import "./aboutus.scss";
 
 //component that displays all the information for each users
 
-
-
 const AboutUs = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-    useEffect(() => {
-        window.scrollTo(0,0)
-    }, [])
-   
-    return(
-        <>
-        <div className="aboutUsHero">
-            <div className="aboutUsContainer">
-                <div className="aboutUsH1Div">
-                    <h1 className="aboutUsH1">Meet the team</h1>
+  return (
+    <>
+      <div className="aboutUsHero">
+        <div className="aboutUsContainer">
+          <div className="aboutUsH1Div">
+            <h1 className="aboutUsH1">Meet the team</h1>
 
-                    <div className='aboutUsADiv'>
-                        <a className='labslink' href='#labs19'>Labs 19</a>
-                        <a className='labslink' href='#labs21'>Labs 21</a>
-                    </div>
-                </div>
+            <div className="aboutUsADiv">
+              <a className="labslink" href="#labs19">
+                Labs 19
+              </a>
+              <a className="labslink" href="#labs21">
+                Labs 21
+              </a>
             </div>
-        </div>     
+          </div>
+        </div>
+      </div>
 
-        <section className='aboutUsSection'>
-            <div className="sectionContainer">
-                <div className="aboutUsH2Div">
-                    <h2 id='management' className="aboutUsH2">Management <span>Team</span></h2>
-                </div>
+      <section className="aboutUsSection">
+        <div className="sectionContainer">
+          <div className="aboutUsH2Div">
+            <h2 id="management" className="aboutUsH2">
+              Management <span>Team</span>
+            </h2>
+          </div>
 
-                <div className="cardContainer">
-                    {team.map(member => {
-                        if (member.management) {
-                            return <TeamCard member={member} />
-                        }
-                    })}
-                </div>
+          <div className="cardContainer">
+            {team.map((member) => {
+              if (member.management) {
+                return <TeamCard member={member} />;
+              } else return <></>;
+            })}
+          </div>
 
-                <div className="aboutUsH2Div">
-                    <h2 id='labs19' className="aboutUsH2">Labs 19 <span>Development Team</span></h2>
-                </div>
+          <div className="aboutUsH2Div">
+            <h2 id="labs19" className="aboutUsH2">
+              Labs 19 <span>Development Team</span>
+            </h2>
+          </div>
 
-                <div className="cardContainer">
-                    {team.map(member => {
-                        if (member.labs === 19 && member.management !== true) {
-                            return <TeamCard member={member} />
-                        }
-                    })}
-                </div>
-                <div className="aboutUsH2Div">
-                    <h2 id='labs21' className="aboutUsH2">Labs 21 <span>Development Team</span></h2>
-                </div>
+          <div className="cardContainer">
+            {team.map((member) => {
+              if (member.labs === 19 && member.management !== true) {
+                return <TeamCard member={member} />;
+              }
+            })}
+          </div>
+          <div className="aboutUsH2Div">
+            <h2 id="labs21" className="aboutUsH2">
+              Labs 21 <span>Development Team</span>
+            </h2>
+          </div>
 
-                <div className="cardContainer">
-                    {team.map(member => {
-                        if (member.labs === 21 && member.management !== true) {
-                            return <TeamCard member={member} />
-                        }
-                    })}
-                </div>
-            </div>
-        </section>
-        <Footer />
-        </>
-    )
-}
+          <div className="cardContainer">
+            {team.map((member) => {
+              if (member.labs === 21 && member.management !== true) {
+                return <TeamCard member={member} />;
+              }
+            })}
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+};
 
-export default AboutUs; 
+export default AboutUs;
