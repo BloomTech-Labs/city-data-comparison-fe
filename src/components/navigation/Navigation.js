5511ea4bc97213717e27c4ae84013c246f488838
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-05-19 18:13:04 -0400

    removes alternate nav bar from meet the team page

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -6,40 +6,8 @@ import { UserContext } from "../../contexts/UserContext";
 import DropMenu from "./Dropdown";
 import { actionColor, navGrey } from "../../utils/cityColors.js";
 
-// All of this code needs refactoring and simplification
-function Navigation(props) {
-  const [offset, setOffset] = useState(0);
-  const [displayNav, setDisplayNav] = useState("show-nav");
-  const [bgColor, setBgColor] = useState("default-color");
-  const [fixedClass, setFixedClass] = useState("");
-  const defaultNavigation = () => {
-    setBgColor("default-color");
-    if (offset === 0) {
-      setDisplayNav("show-nav");
-    } else {
-      if (displayNav === "hide-nav") {
-        setDisplayNav("show-nav");
-      }
-    }
-  };
-
-  let styles = {
-    float: "right",
-  };
-
-  return (
-    <>
-      {props.location.pathname === "/meet-the-team" ? (
-        <MeetTheTeam />
-      ) : (
-        <NavBar />
-      )}
-    </>
-  );
-}
-
 //Nav bar for everthing
-function NavBar() {
+function Navigation() {
   const { user, setUser } = useContext(UserContext);
   const [offset, setOffset] = useState(0);
   const [displayNav, setDisplayNav] = useState("show-nav");
@@ -98,94 +66,6 @@ function NavBar() {
         ) : (
           <>
             {/* DROPDOWN NAVBAR MENU FOR MOBILE STARTS HERE */}
-            <DropMenu />
-          </>
-        )}
-      </nav>
-      {user == null ? (
-        <>
-          <div className="dropdownContainer">
-            <div className="dropdown" style={styles}>
-              <button className="dropbtn">Menu</button>
-              <div className="dropdown-content">
-                <Link to="/compare">Explore</Link>
-                <Link to="/signin">Log In</Link>
-                <Link to="/signup">Get Started</Link>
-              </div>
-            </div>
-          </div>
-        </>
-      ) : (
-        <div className="dropdownContainer">
-          <DropMenu />
-        </div>
-      )}
-    </div>
-  );
-}
-
-//Nav bar for meet the team Page
-function MeetTheTeam() {
-  const { user, setUser } = useContext(UserContext);
-  const [offset, setOffset] = useState(0);
-  const [displayNav, setDisplayNav] = useState("show-nav");
-  const [bgColor, setBgColor] = useState("default-color");
-  const [fixedClass, setFixedClass] = useState("");
-  const defaultNavigation = () => {
-    setBgColor("default-color");
-    if (offset === 0) {
-      setDisplayNav("show-nav");
-    } else {
-      if (displayNav === "hide-nav") {
-        setDisplayNav("show-nav");
-      }
-    }
-  };
-
-  let styles = {
-    float: "right",
-  };
-
-  return (
-    <div
-      className={
-        "navigation-container " +
-        bgColor +
-        `main-nav ${displayNav} ${fixedClass}`
-      }
-      onMouseEnter={() => {
-        defaultNavigation();
-      }}
-      onMouseLeave={() => {
-        defaultNavigation();
-      }}
-    >
-      <a className="header-logo" href="/">
-        {" "}
-        <img className="mock-logo" src={citrics} alt="logo" />
-      </a>
-      <nav className="main-nav">
-        {user ? <div /> : null}
-        <Link to="/compare" className="nav-button" style={{ color: "white" }}>
-          Explore
-        </Link>
-        {user == null ? (
-          <>
-            <Link
-              to="/compare"
-              className="nav-button"
-              style={{ color: "white" }}
-            >
-              Compare
-            </Link>
-            <Link id="login-link" to="/signin">
-              <img alt="lock" src={signInLock} />
-              Log In
-            </Link>
-            {/* <Link id="signup-link" to="/signup">Get Started</Link> */}
-          </>
-        ) : (
-          <>
             <DropMenu />
           </>
         )}
