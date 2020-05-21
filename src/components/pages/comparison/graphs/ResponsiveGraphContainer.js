615424087987396bb1a4501bdc2e73381e9680ea
liampmoore <16942895+liampmoore@users.noreply.github.com>
2020-05-21 15:47:38 -0400

    adds responsive container for graphs to prevent text warping in mobile

---
+++ src/components/pages/comparison/graphs/ResponsiveGraphContainer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+function ResponsiveGraphContainer(props) {
+  const StyledResponsiveGraphContainer = styled.div`
+    min-height: 300px;
+
+    @media screen and (min-width: 600px) {
+      min-height: 400px;
+    }
+    @media screen and (min-width: 800px) {
+      min-height: 500px;
+    }
+  `;
+
+  return (
+    <StyledResponsiveGraphContainer>
+      {props.children}
+    </StyledResponsiveGraphContainer>
+  );
+}
+
+export default ResponsiveGraphContainer;
