ff8dd8a24c050d26da473faa9d5c7f062f976900
David Pok <34250625+david-pok@users.noreply.github.com>
2020-07-25 15:48:30 -0700

    Update Callback.js

--- src/components/Callback.js
+++ src/components/Callback.js
@@ -1,20 +1,18 @@
 import React, { useContext } from "react";
 import queryString from "query-string";
 import { Redirect } from "react-router-dom";
-import { UserContext } from "../contexts/UserContext";
-import ReactGA from "react-ga";
-
+import { useDispatch } from "react-redux";
+import { setUser } from "../../../redux/actions/userActions";
 // This component is necessary for OAuth.
 // This route is where the user is redirected after using an OAuth provider.
 // The component uses the redirect query paramaters in the url to save a token to localStorage.
 const Callback = (props) => {
-  const { setUser } = useContext(UserContext);
+  const dispatch = useDispatch();
   const values = queryString.parse(props.location.search);
-  ReactGA.event({ category: "User", action: "Logged in via Oauth" });
-  localStorage.setItem("jwt", values.jwt);
-  let newUser = JSON.parse(values.user);
-  setUser(newUser);
+  const jwt = values.jwt;
+  const user = JSON.parse(values.user);
+  dispatch(setUser(jwt, user));
   return <>{Redirect("/")}</>;
 };
-
+  
 export default Callback;
